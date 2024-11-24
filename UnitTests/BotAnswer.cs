20f2b928981fa0fcc85ade1bd8fa4d2f9c339585
Александр <Volosnikov.aleksandr2@gmail.com>
2024-11-25 04:18:36 +0500

    add unitTests proj

---
+++ UnitTests/BotAnswer.cs
@@ -0,0 +1,19 @@
+﻿namespace UnitTests
+{
+    [TestClass]
+    public sealed class BotAnswer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+    }
+}

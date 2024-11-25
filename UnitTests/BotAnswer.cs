44e9180fb6076f9b75b790aebceafbfc1920c569
Александр <Volosnikov.aleksandr2@gmail.com>
2024-11-25 13:29:42 +0500

    Add some example tests

--- UnitTests/BotAnswer.cs
+++ UnitTests/BotAnswer.cs
@@ -1,4 +1,6 @@
-﻿namespace UnitTests
+﻿using BotService;
+
+namespace UnitTests
 {
     [TestClass]
     public sealed class BotAnswer
@@ -6,14 +8,32 @@
         [TestMethod]
         public void TestMethod1()
         {
+            string expected = "Привет";
+            string input = "Hello";
+
+            string answer = BOT.SendMessage(input).Message;
+
+            Assert.AreEqual(expected, answer);
         }
         [TestMethod]
         public void TestMethod2()
         {
+            string expected = "Как дела";
+            string input = "Normalno";
+
+            string answer = BOT.SendMessage(input).Message;
+
+            Assert.AreEqual(expected, answer);
         }
         [TestMethod]
         public void TestMethod3()
         {
+            string expected = "Команды не существует";
+            string input = ":3";
+
+            string answer = BOT.SendMessage(input).Message;
+
+            Assert.AreEqual(expected, answer);
         }
     }
 }
